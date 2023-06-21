a1a87a6bc03a370ac6b111ea3ade964d3348cca2
sweta20singh2004 <135997300+sweta20singh2004@users.noreply.github.com>
2023-06-21 18:36:42 +0000

    six programs complete

---
+++ informatics-practices/chapter-3/3.4.py
@@ -0,0 +1,4 @@
+weight_in_kg = float(input("Enter weight in kg :"))
+height_in_metre = float(input("Enter height in metres :"))
+bmi = weight_in_kg /( height_in_metre * height_in_metre )
+print("BMI is :" ,bmi)
---
+++ informatics-practices/chapter-3/3.6.py
@@ -0,0 +1,4 @@
+km = int(input("Enter kilometres :"))
+miles = km * 0.621371
+print("kilometres :", km)
+print("Miles :", miles)
