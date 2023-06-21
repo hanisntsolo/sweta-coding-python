1504961d251ac84932fae732f132ee2529c5c58b
sweta20singh2004 <135997300+sweta20singh2004@users.noreply.github.com>
2023-06-21 17:36:55 +0000

    five programs added

---
+++ informatics-practices/chapter-3/3.2.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter number 1 :"))
+num2=int(input("Enter number 2 :"))
+num3=int(input("Enter number 3 :"))
+S9um= num1+num2+num3
+print("Three numbers are :",num1, num2, num3)
+print("Sum is :", Sum)
