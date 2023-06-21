3fc712b69fbee2bd5ce95c4d720c039b022c9dac
sweta20singh2004 <135997300+sweta20singh2004@users.noreply.github.com>
2023-06-21 18:01:21 +0000

    five programs added

---
+++ informatics-practices/chapter-3/3.3.py
@@ -0,0 +1,8 @@
+length = float(input("Enter length of the rectangle :"))
+breadth =float(input("Enter breadth of the rectangle :"))
+area = length*breadth
+print ("rectangle specifications")
+print ("Length = ", breadth)
+print ("breadth = " , breadth)
+print ("Area = ", area)
+
